2cb88303a5b9797f665c31ab4a8276bfad8fbca7
mamoanwar97 <mamoanwar97@gmail.com>
2020-05-13 01:59:47 +0200

    unit test Todo item

--- tests/unit/todo.spec.js
+++ tests/unit/todo.spec.js
@@ -3,6 +3,42 @@ import Todo from "@/components/Todo.vue";
 import CreateTodo from "@/components/CreateTodo.vue";
 import TodoList from "@/components/TodoList_test.vue";
 
+describe("Todo.vue", () => {
+  const description = "this test";
+  const completed = true;
+  const wrapper = shallowMount(Todo, {
+    propsData: { description, completed }
+  });
+
+  it("Renders props.description when passed", () => {
+    expect(wrapper.find("span").text()).toBe("this test");
+  });
+
+  it("Set isEditing to false by default", () => {
+    expect(wrapper.vm.$data["isEditing"]).toBe(false);
+  });
+
+  it("Edit button invert isEditing value", () => {
+    wrapper.find(".editing").trigger("click");
+    expect(wrapper.vm.$data["isEditing"]).toBe(true);
+  });
+
+  it("Editing emits the new description", () => {
+      wrapper.vm.$data["newTodoDescription"] = "ahmed"
+      wrapper.find(".editing").trigger("click");
+      expect(wrapper.emitted()["on-edit"][0][0]).toBe("ahmed");
+  });
+
+    it("Checkbox emits the toggle function", () => {
+        wrapper.find(".checkbox").trigger("change");
+        expect(wrapper.emitted()["on-toggle"]).toBeTruthy()
+    });
+
+    it("Delete emits the delete function", () => {
+        wrapper.find(".delete").trigger("click");
+        expect(wrapper.emitted()["on-delete"]).toBeTruthy()
+    });
+});
 
 describe("CreateTodo.vue", () => {
 
